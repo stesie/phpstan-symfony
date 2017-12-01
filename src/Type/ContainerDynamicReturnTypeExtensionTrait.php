2c728e639e7abf3c52b10b95eeffb4a0b0184230
Stefan Siegl <stefan.siegl@mayflower.de>
2017-12-01 10:54:33 +0100

    extract ContainerDynamicReturnTypeExtensionTrait

---
+++ src/Type/ContainerDynamicReturnTypeExtensionTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Lookyman\PHPStan\Symfony\Type;
+
+use Lookyman\PHPStan\Symfony\ServiceMap;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Scalar\String_;
+
+trait ContainerDynamicReturnTypeExtensionTrait
+{
+
+	/**
+	 * @var ServiceMap
+	 */
+	private $serviceMap;
+
+	public function __construct(ServiceMap $symfonyServiceMap)
+	{
+		$this->serviceMap = $symfonyServiceMap;
+	}
+
+	public function getTypeFromMethodCall(
+		MethodReflection $methodReflection,
+		MethodCall $methodCall,
+		Scope $scope
+	): Type {
+		$services = $this->serviceMap->getServices();
+		return isset($methodCall->args[0])
+			&& $methodCall->args[0] instanceof Arg
+			&& $methodCall->args[0]->value instanceof String_
+			&& \array_key_exists($methodCall->args[0]->value->value, $services)
+			&& !$services[$methodCall->args[0]->value->value]['synthetic']
+			? new ObjectType($services[$methodCall->args[0]->value->value]['class'])
+			: $methodReflection->getReturnType();
+	}
+
+}
